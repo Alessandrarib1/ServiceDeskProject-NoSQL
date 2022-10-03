b41a25d2a4254024ff60fe7dd08db3215d4cf722
Matudor7 <tdr.nsk@gmail.com>
2022-10-03 16:57:49 +0200

    Revert "made employee DAL and employee Model"
    This reverts commit cc8000149762c880c7542706340c6f74457c9511.

--- GardenModel/Class1.cs
+++ GardenModel/Class1.cs
@@ -4,13 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GardenDAL
+namespace GardenModel
 {
-    class EmployeeDao : BaseDao
+    public class Class1
     {
-        public EmployeeDao() : base()
-        {
-
-        }
     }
 }
