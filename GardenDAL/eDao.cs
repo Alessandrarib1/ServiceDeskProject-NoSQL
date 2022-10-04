59d2946f8965f40a77e16a08dceccba197f2e091
Tudor <tdr.nsk@gmail.com>
2022-10-04 16:32:59 +0200

    Create eDao.cs

---
+++ GardenDAL/eDao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GardenDAL
+{
+    class eDao //naming this class "employeeDao" causes errors.
+    {
+        public eDao() : base()
+        {
+
+        }
+    }
+}
