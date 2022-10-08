7c5001e9a93857223d036b4f256ced2581c61707
Ign6779 <ignacio.deeusebio.cobo@gmail.com>
2022-10-08 12:54:47 +0200

    Basic model added

---
+++ GardenModel/Ticket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GardenModel
+{
+    public class Ticket
+    {
+        private int employeeNumber { get; set; }
+        private Category category { get; set; }
+        public Status status { get; set; }
+        public string description { get; set; }
+
+        public Ticket(int employeeNuber, Category category, Status status, string description)
+        {
+            this.employeeNumber = employeeNumber;
+            this.category = category;
+            this.status = status;
+            this.description = description;
+        }
+    }
+}
---
+++ GardenModel/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GardenModel
+{
+    public class User
+    {
+        private string username { get; set; }
+        private string password { get; set; }
+        private int employeeNumber { get; set; }
+        private Job job { get; set; }
+
+        public User(string username, string password, int employeeNumber, Job job)
+        {
+            this.username = username;
+            this.password = password;
+            this.employeeNumber = employeeNumber;
+            this.job = job;
+        }
+    }
+}
